e1dedd28ec83e30bfe4ea68a429a9ee2585f79ab
Dennis Ngugi <80087018+DennisN7@users.noreply.github.com>
2024-08-25 16:09:08 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,26 +4,24 @@ import { config } from "@/config";
 import { signOgImageUrl } from "@/lib/og-image";
 import Markdown from "react-markdown";
 
-// Import your local image
-import profileImage from "@/public/images/profile.jpg";
+const content = # About Me
 
-const content = `# About Me
+![Samantha](https://imagedelivery.net/lLmNeOP7HXG0OqaG97wimw/clvlugru90000o4g8ahxp069s/db7abbe3-aa5c-433e-a16d-cbf137d1c9e5.png/public)
 
-![Dennis](${profileImage})
+Hey there! I'm Dennis Ngugi, a 23-year-old Sotfware Engineer and Dev Ops student. When  I am not diving into code creating and deploying systems you will find me immersed in the world of cars. My passion for technology and cars seemlesly blend in and rev up my love for the automotive industry.
 
-Hey there! I'm Dennis Ngugi, a 23-year-old Software Engineer and Dev Ops student. When I am not diving into code creating and deploying systems you will find me immersed in the world of cars. My passion for technology and cars seamlessly blend in and rev up my love for the automotive industry.
+Cars are more than just machines, they are a fusion of engineering, art and raw emotions and whether it is the thrill of the roar of an engine or a perfectly executed deployment, I am always on the lookout for what's next.
 
-Cars are more than just machines, they are a fusion of engineering, art, and raw emotions. Whether it's the thrill of the roar of an engine or a perfectly executed deployment, I am always on the lookout for what's next.
 
-This blog is where my two worlds collide, and I will be offering a different perspective on the latest automation technology, engineering marvels, and the joy of driving. You'll also find a mix of the latest technology trends and my personal reflections and favorite machines.
+This blog is where my two worlds collide and I will be offering a different perspective in the latest automation technology, enginnering marvels and the joy of driving. You will also find a mix of the latest technology trends and my personal reflections and favorite machines. 
 
-Whether I'm exploring the parallels of the world of cloud architecture or sharing my thoughts about a certain car, my goal is to deliver great insight and perspective for the tech bros and gear heads (and also get myself a Volkswagen GTI :) 
+Whether I am exploring the parrallels of the world of cloud architecture or sharing my thoughts about a certain car, my goal is to deliver great insight and perspective for the tech bros and gear heads(and also get myselt a Volkwagen GTI :)
 
 So join me on this adventure, and let's drive and deploy.
 
 Love,
 
-Dennis Ngugi`;
+Dennis Ngugi;
 
 export async function generateMetadata() {
   return {
